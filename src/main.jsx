ee0288eda6de9d023550626b669e0c8211cd4547
Abdulbasit Fasasi <kvngfax23@gmail.com>
2023-10-03 19:45:24 +0100

    kk

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode basename={process.env.PUBLIC_URL}>
     <App />
   </React.StrictMode>,
 )
